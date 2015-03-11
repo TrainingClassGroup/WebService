c48f3075f12efbd72e0e12fc6125a605bfa4367a
zhaojietong <zhaojietong@sohu.com>
2015-03-11 16:21:39 +0800

    *

--- config/db.php
+++ config/db.php
@@ -7,7 +7,7 @@ return [
             // SQL Server: mssql:host=localhost;dbname=testdb
             // SQL Server: sqlsrv:server=localhost;Database=testdb // for 2000,2005,2008
             // Oracle: oci:dbname=//localhost:1521/testdb
-            'dsn' => 'pgsql:host=115.28.76.20;port=5432;dbname=mydb',
+            'dsn' => 'pgsql:host=;port=5432;dbname=mydb',
             'username' => 'postgres',
             'password' => 'postgres',
             'charset' => 'utf8'
